bebc8135653fd63af87b3a066162a0c1ea8711ee
Livia <livialucizano22@gmail.com>
2026-04-28 08:57:11 -0300

    refactor: organiza a estrutura do backend

---
+++ firebase_server/functions/src/startups/index.ts
@@ -0,0 +1,5 @@
+export { addBatchStartups } from "./handlers/addBatchStartups";
+
+export { getStartups } from "./handlers/getStartups";
+
+export { getStartupsTest } from "./handlers/getStartupsTest";
