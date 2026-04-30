0096fb796dc93f5d15e5173ad9ff42618c1decb1
Bernardooficial123 <bernardooficial2025@gmail.com>
2026-04-30 17:33:27 -0300

    Refactor Catalogo to use StatefulWidget and load data

--- mescla_invest_app/lib/features/catalog/presentation/screens/startup_catalog_screen.dart
+++ mescla_invest_app/lib/features/catalog/presentation/screens/startup_catalog_screen.dart
@@ -1,351 +1,164 @@
 /* Autor: Bernardo Castro Brandão de Oliveira */
 
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:mescla_invest_app/features/auth/data/repositories/auth_repository.dart';
-import '../theme/background_app.dart';
+import 'package:flutter/material.dart'; //Importa o framework principal do Flutter (widgets, UI, Scaffold, etc)
+import 'package:google_fonts/google_fonts.dart'; //Permite usar fontes externas (como Montserrat) no app.
+import 'package:mescla_invest_app/features/auth/data/repositories/auth_repository.dart'; //Importa um arquivo do seu projeto (provavelmente usado para login/logout)
+import '../theme/background_app.dart'; //Importa um widget personalizado de fundo (BackgroundContainer).
+import 'package:cloud_firestore/cloud_firestore.dart'; //Importa o Firebase Firestore (banco de dados).
 
-class MesclaInvest extends StatelessWidget {
-  const MesclaInvest({super.key});
+class MesclaInvest extends StatelessWidget { //Define o app principal como imutável (Stateless).
+  const MesclaInvest({super.key}); //Construtor da classe.
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Catalogo(),
-      theme: ThemeData(
-        textTheme: GoogleFonts.montserratTextTheme(),
+  @override //Indica que você está sobrescrevendo um método da classe pai.
+  Widget build(BuildContext context) { //Método obrigatório que constrói a interface
+    return MaterialApp( //Cria o app com Material Design
+      debugShowCheckedModeBanner: false, //Remove o banner "DEBUG"
+      home: Catalogo(), //Define a tela inicial como Catalogo
+      theme: ThemeData( //Define o tema do app
+        textTheme: GoogleFonts.montserratTextTheme(), //Aplica a fonte Montserrat globalmente
+        
       ),
     );
   }
 }
 
-class CardCatalogo {
+class CardCatalogo { //Classe que representa uma startup
   final String nome_startup;
-  final String logoImg;
+  final String data_criacao;
   final String mini_descricao;
-  final String status;
+  final String estagio_stp;
+  final String status_stp;
+  final int tokens_disponiveis;
   final int tokens_emitidos;
-
-  final List<String> socios;
+  final double valorFixo_token;
   final double capital_aportado;
+  final String setor_stp;
+  final String? video_demoURL;
+  final List<String> socios_stp;
+  final List<String> ment_conselho;
+  final List<int> ofetas_ativas;
+  final List<String> part_societaria;
 
-  CardCatalogo({
+  CardCatalogo({ //Construtor da classe
     required this.nome_startup,
-    required this.logoImg,
+    required this.data_criacao,
     required this.mini_descricao,
-    required this.status,
+    required this.estagio_stp,
+    required this.status_stp,
+    required this.tokens_disponiveis,
     required this.tokens_emitidos,
-    required this.socios,
+    required this.valorFixo_token,
     required this.capital_aportado,
-  });
+    required this.setor_stp,
+    this.video_demoURL,
+    required this.socios_stp,
+    required this.ment_conselho,
+    required this.ofetas_ativas,
+    required this.part_societaria,
+      });
+
+  factory CardCatalogo.fromFirestore(Map<String, dynamic> data) { //Construtor especial que transforma dados do Firebase em objeto Dart.
+  return CardCatalogo( //Cria um novo objeto
+    nome_startup: data['nome_startup'] ?? '', //Se existir usa, senão usa string vazia
+    data_criacao: data['data_criacao'] ?? '',
+    mini_descricao: data['mini_descricao'] ?? '',
+    estagio_stp: data['estagio_stp'] ?? '',
+    status_stp: data['status_stp'] ?? '',
+    tokens_disponiveis: data['tokens_disponiveis'] ?? 0,
+    tokens_emitidos: data['tokens_emitidos'] ?? 0,
+    valorFixo_token: (data['valorFixo_token'] ?? 0).toDouble(), //Garante que o valor seja double
+    capital_aportado: (data['capital_aportado'] ?? 0).toDouble(),
+    setor_stp: data['setor_stp'] ?? '',
+    video_demoURL: data['video_demoURL'],
+    socios_stp: List<String>.from(data['socios_stp'] ?? []), //Converte lista dinâmica em lista de String
+    ment_conselho: List<String>.from(data['ment_conselho'] ?? []),
+    ofetas_ativas: List<int>.from(data['ofetas_ativas'] ?? []),
+    part_societaria: List<String>.from(data['part_societaria'] ?? []),
+  );    
 }
 
-class Catalogo extends StatelessWidget {
-  const Catalogo({super.key});
+}
+
+class Catalogo extends StatefulWidget { //Tela que muda dinamicamente
+  const Catalogo({super.key}); //Construtor da classe
 
   @override
-  Widget build(BuildContext context) {
-    final List<CardCatalogo> startups = [
-      CardCatalogo(
-        nome_startup: "NotaCerta LTDA",
-        logoImg: "assets/images/logo_notacerta.png",
-        mini_descricao: "Plataforma de aulas de música",
-        status: "Em Operação",
-        tokens_emitidos: 100000,
-        socios: [
-          "Livia Lucizano - 50%",
-          "Laura Soares - 50%",
-        ],
-        capital_aportado: 70000,
-      ),
-
-      CardCatalogo(
-        nome_startup: "HealthVibe LTDA",
-        logoImg: "assets/images/logo_healthvibe.png",
-        mini_descricao: "Aplicativo de telemedicina focado em saúde mental para estudantes.",
-        status: "Em Operação",
-        tokens_emitidos: 1000000,
-        socios: [
-          "Beatriz Fernandes Costa - 100%",
-        ],
-        capital_aportado: 50000,
-      ),
-
-      CardCatalogo(
-        nome_startup: "MetaLive LTDA",
-        logoImg: "assets/images/logo_metalive.png",
-        mini_descricao: "Aplicativo de integração de realidade aumentada no ambiente metaverso",
-        status: "Em Operação",
-        tokens_emitidos: 100000,
-        socios: [
-          "Moski Shimoji - 25%",
-          "Abran Lincher - 25%",
-          "Erick Lujahini - 15%",
-          "Emay Saltgate - 15%",
-          "Truham Wilson - 10%",
-          "Maly Salzburg - 10%",
-        ],
-        capital_aportado: 89000,
-      ),
-
-      CardCatalogo(
-        nome_startup: "CardVIsion LTDA",
-        logoImg: "assets/images/logo_cardvision.png",
-        mini_descricao: "Plataforma de valorização de cartas colecionáveis da cultura Geek",
-        status: "Em Operação",
-        tokens_emitidos: 500000,
-        socios: [
-          "Gabriela Silva - 50%",
-          "Lucas Mendes - 50%",
-        ],
-        capital_aportado: 60000,
-      ),
-    ];
-
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        title: const Text("Catálogo"),
-        foregroundColor: const Color(0xFF353988),
-        actions: [
-          TextButton.icon(
-            onPressed: AuthRepository.instance.logout,
-            label: Icon(Icons.logout)
-          )
-        ],
-      ),
-
-      body: BackgroundContainer(
-        child: ListView.builder(
-          itemCount: startups.length,
-          itemBuilder: (context, index) {
-            return CardStartup(s: startups[index]);
-          },
-        ),
-      ),
-    );
-  }
+  State<Catalogo> createState() => _CatalogoState(); //Cria o estado da tela.
 }
 
-class BotaoIrPara extends StatelessWidget {
-  final Widget pagina;
-  final String texto;
+class _CatalogoState extends State<Catalogo> { //Classe que controla comportamento da tela.
+  final List<CardCatalogo> startups = []; //Lista de startups carregadas.
+  final ScrollController controller = ScrollController(); //Controla a rolagem da lista.
 
-  const BotaoIrPara({
-    super.key,
-    required this.pagina,
-    this.texto = "Ver mais",
-  });
+  QueryDocumentSnapshot? lastDoc; //Guarda o último documento (para paginação).
+  bool carregando = false; //Indica se está carregando dados.
+  bool acabou = false; //Indica se não há mais dados.
 
   @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF353988),
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => pagina),
-        );
-      },
-      child: Text(texto),
-    );
-  }
-}
+  void initState() { //Executa quando a tela inicia.
+    super.initState();//Inicializa corretamente a classe pai.
+    carregarMais(); //Carrega os primeiros dados
 
-class CardStartup extends StatefulWidget {
-  final CardCatalogo s;
-
-  const CardStartup({super.key, required this.s});
-
-  @override
-  State<CardStartup> createState() => _CardStartupState();
-}
-
-class _CardStartupState extends State<CardStartup> {
-  bool expandido = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final s = widget.s;
-
-    return Card(
-      color: const Color(0xFFE8E9EB),
-      margin: const EdgeInsets.all(6),
-      child: Padding(
-        padding: const EdgeInsets.all(6),
-        child: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage(s.logoImg),
-                ),
-
-                const SizedBox(width: 12),
-
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              s.nome_startup,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-
-                          IconButton(
-                            icon: Icon(
-                              expandido
-                                  ? Icons.close_fullscreen
-                                  : Icons.open_in_full,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                expandido = !expandido;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Text(s.mini_descricao),
-
-                      const SizedBox(height: 8),
-
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 4,
-                        ),
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFDADADA),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Text(
-                          s.status,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-
-                      AnimatedCrossFade(
-                        firstChild: const SizedBox(),
-                        secondChild: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(height: 10),
-
-                            const Text(
-                              "Participação (p/sócio):",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-
-                            const SizedBox(height: 4),
-
-                            ...s.socios.map((socio) => Text("• $socio")),
-
-                            const SizedBox(height: 10),
-
-                            const Text(
-                              "Capital Aportado:",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-
-                            Text("R\$ ${s.capital_aportado.toStringAsFixed(0)}"),
-                          ],
-                        ),
-                        crossFadeState: expandido
-                            ? CrossFadeState.showSecond
-                            : CrossFadeState.showFirst,
-                        duration: const Duration(milliseconds: 300),
-                      ),
-
-                      const SizedBox(height: 10),
-
-                      const Text("Tokens emitidos:"),
-                      Text("${s.tokens_emitidos} tokens"),
-
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: BotaoIrPara(
-                          pagina: const Placeholder(),
-                        ),
-                      ),
-                    ],
-
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class Pesquisar extends StatefulWidget {
-  const Pesquisar({super.key});
-
-  @override
-  State<Pesquisar> createState() => _PesquisarState();
-}
-
-class _PesquisarState extends State<Pesquisar> {
-  final TextEditingController _controller = TextEditingController();
-
-  String resultado = "";
-
-  void _buscar() {
-    setState(() {
-      resultado = _controller.text;
+    controller.addListener(() { //Escuta a rolagem
+      if (controller.position.pixels == 
+          controller.position.maxScrollExtent && 
+          !carregando &&
+          !acabou) {  //verifica se chegou no fim da lista
+        
+        carregarMais(); //Carrega mais dados, se não estiver carregando e ainda tem dados
+      }
     });
   }
 
+  Future<void> carregarMais() async { //Função assíncrona para mostrar o carregamento
+    setState(() => carregando = true); //Atualiza a tela para mostrar que está carregando
+
+    Query query = FirebaseFirestore.instance //Busca 20 documentos do Firebase.
+        .collection('startups')
+        .limit(20); //Coloca o limite de 20 aqui!
+
+    if (lastDoc != null) { //Veriffica se já carregou antes
+      query = query.startAfterDocument(lastDoc!);//Continua a partir do último documento
+    }
+
+    final snapshot = await query.get(); //Execulta a query (pega os dados do Firebase)
+
+    if (snapshot.docs.isEmpty) { 
+      acabou = true; //Se não vier nada, acaba a exibição
+    } else { 
+      final novos = snapshot.docs.map((doc) { //Caso contrário, Converte documentos em objetos
+        return CardCatalogo.fromFirestore(
+          doc.data() as Map<String, dynamic>,
+        );
+      }).toList(); //Transforma em lista
+
+      startups.addAll(novos); //Adiciona novos itens à lista existente.
+      lastDoc = snapshot.docs.last; //Atualiza o último documento
+    }
+
+    setState(() => carregando = false); //Remove loading
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pesquisar Startup')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: "Nome da startup",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _buscar,
-              child: const Text("Pesquisar"),
-            ),
-            const SizedBox(height: 20),
-            Text("Resultado: $resultado"),
-          ],
+  Widget build(BuildContext context) { //Constrói o design
+    return Scaffold( //Estrutura base da tela
+      appBar: AppBar(title: const Text('Catálogo')), //Barra superior com título
+      body: BackgroundContainer( //Fundo personalizado
+        child: ListView.builder( //Lista dinâmica
+          controller: controller, //Liga a rolagem ao controller
+          itemCount: startups.length + 1, //+1 para mostrar loading no final.
+          itemBuilder: (context, index) { //Função que cria cada item.
+            if (index < startups.length) {
+              return CardStartup(s: startups[index]); //Se ainda for item normal mostra o card
+            } else {
+              return carregando //Caso contrário, se estiver carregando, mostra espaço com padding.
+                  ? const Padding(
+                      padding: EdgeInsets.all(16), //Espaçamento
+                      child: Center(child: CircularProgressIndicator()), //Mostra o Girador de Carregamento
+                    )
+                  : const SizedBox(); //Se não estiver carregando, não mostra nada.
+            }
+          },
         ),
       ),
     );
