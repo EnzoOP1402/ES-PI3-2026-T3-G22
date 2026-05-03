01c136cf86317be623de199a5b012af59591ec39
Livia <livialucizano22@gmail.com>
2026-05-03 03:41:17 -0300

    backend detalhes startup

--- firebase_server/functions/src/startups/index.ts
+++ firebase_server/functions/src/startups/index.ts
@@ -3,3 +3,5 @@ export { addBatchStartups } from "./handlers/addBatchStartups";
 export { getStartups } from "./handlers/getStartups";
 
 export { getStartupsTest } from "./handlers/getStartupsTest";
+
+export { getStartupDetails } from "./handlers/getStartupDetails";
